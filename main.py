63d59fec7fcac21543832f0f55aa3ef28dbe5e39
DanAlcalde <danalcalde@gmail.com>
2025-04-03 15:54:29 -0400

    Second push, updates to movement and asteroids

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-
-
-
+from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
  
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-    screen.fill("black")
-    pygame.display.flip()
+        
+        updatable.update(dt)    
+        
+        screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
+
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000  
+        
     
  
 if __name__ == "__main__":
